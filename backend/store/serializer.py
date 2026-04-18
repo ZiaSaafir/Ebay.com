628ad67ab01ff71153ab2be27000886e1ae73e62
Ziaullah-faizaninnovator <ziagit11@gmail.com>
2026-04-18 19:12:08 +0500

    Fix product image full URL

--- backend/store/serializer.py
+++ backend/store/serializer.py
@@ -21,18 +21,15 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = '__all__'
 
     def get_image(self, obj):
         request = self.context.get("request")
-
         if obj.image:
             if request:
                 return request.build_absolute_uri(obj.image.url)
             return obj.image.url
-
         return None
-
 
 # ==========================
 # CART ITEM
