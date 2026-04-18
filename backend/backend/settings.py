b05a2e7fa392a65645b34f05bac9dbf013616a8d
Ziaullah-faizaninnovator <ziagit11@gmail.com>
2026-04-18 15:19:19 +0500

    add build.sh

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 DEBUG = False
 
 #ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
